bbfef01aed65faa22c44c13d36b08c7044e6beaa
Damian <damiangalvan66@gmail.com>
2024-09-16 09:21:41 -0600

    feat(globals.lua): add custom Vim settings for line numbering and indentation to enhance user experience

--- lua/config/globals.lua
+++ lua/config/globals.lua
@@ -3,3 +3,17 @@
 -- This is also a good place to setup other settings (vim.opt)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+
+-- vim customs
+local opt = vim.opt
+opt.relativenumber = true
+opt.number = true
+
+-- tabs & indentation
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.autoindent = true
+
+
+
