dfaaf727ed16657e4543b623a2a9ad35d5ddeab5
Damian <damiangalvan66@gmail.com>
2025-08-04 10:37:19 -0600

    update configs

--- lua/plugins/nightfox.lua
+++ lua/plugins/nightfox.lua
@@ -1,9 +1,14 @@
+-- Archivo de configuración de lazy.nvim (init.lua o el archivo adecuado)
 return {
-        "EdenEast/nightfox.nvim",
-        lazy = false,
-        priority = 999,
-        config = function()
-                vim.cmd('colorscheme nightfox')
-        end
-
+    'catppuccin/nvim',
+    lazy = false, -- Cargar inmediatamente al iniciar Neovim
+    priority = 1000, -- Ajusta la prioridad para asegurar que el tema se cargue correctamente
+    config = function()
+        require('catppuccin').setup({
+            flavour = 'mocha', -- Otras opciones: latte, frappe, macchiato
+            transparent_background = false, -- Puedes habilitar fondo transparente si lo prefieres
+        })
+        vim.cmd('colorscheme catppuccin') -- Aplica el esquema de colores
+    end
 }
+
